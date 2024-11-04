9e121d1909651324a20fd581a944c11c441e3bcc
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-11-04 16:07:00 +0200

    modal

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -21,4 +21,17 @@ galleryListEl.insertAdjacentHTML("beforeend", markup);
 
 function onGalleryClick(e) {
   console.log(e.target.dataset.origin);
+
+  if (!e.target.classList.contains("gallery__image")) {
+    console.log("chepuha");
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+     <div>
+      <img src="${e.target.dataset.origin}" alt="">
+    </div>
+`);
+
+  instance.show();
 }
