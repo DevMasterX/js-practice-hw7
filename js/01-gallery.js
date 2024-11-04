5a8a818621496415e9c82b3892672e4b618c28b1
DevMasterX <126589591+DevMasterX@users.noreply.github.com>
2024-11-04 14:50:59 +0200

    494

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector(".gallery");
+
+galleryListEl.addEventListener("click", onGalleryClick);
+
+const markup = galleryItems
+  .map(({ preview, description, original }) => {
+    return `
+  <li class="gallery__item">
+      <img class="gallery__image"  src="${preview}" alt="${description}" data-origin='${original}'/>
+  </li> 
+        `;
+  })
+  .join("");
+
+galleryListEl.insertAdjacentHTML("beforeend", markup);
+
+function onGalleryClick(e) {
+  console.log(e.target.dataset.origin);
+}
